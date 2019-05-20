d7e94af1a5a031df6ee349cfa79e003a3bbb80d0
YeonJung Kim <duswnd25@gmail.com>
2019-05-20 19:25:18 +0900

    파일명 변경

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -6,9 +6,21 @@ const util = reqlib('/src/core/utils.js');
 
 const router = express.Router();
 
+const clientId = 'mCcaI95vzy';
+
 // 기본
 router.get('/', async (req, res) => {
-	const clientId = 'mCcaI95vzy';
+	const clientDataArray = await dbManager.getClientData(clientId, 50);
+	const data = {
+		client: [clientId],
+		log: util.generateDisplayData(clientDataArray)
+	};
+	data.page = 'index';
+	return res.status(200).render('layout', { data });
+});
+
+// 실시간
+router.get('/current', async (req, res) => {
 	const clientDataArray = await dbManager.getClientData(clientId, 50);
 	const data = {
 		client: [clientId],
@@ -32,7 +44,6 @@ router.get('/total', async (req, res) => {
 
 // 전체
 router.get('/table', async (req, res) => {
-	const clientId = 'mCcaI95vzy';
 	const clientDataArray = await dbManager.getClientData(clientId, 0);
 	const data = {
 		client: [clientId],
@@ -43,8 +54,7 @@ router.get('/table', async (req, res) => {
 });
 
 // 시간별
-router.get('/hour', async (req, res) => {
-	const clientId = 'mCcaI95vzy';
+router.get('/halftime', async (req, res) => {
 	const clientDataArray = await dbManager.getClientHourlyData(clientId);
 	const data = {
 		client: [clientId],
